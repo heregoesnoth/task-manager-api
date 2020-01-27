b1dcebe43640f7a00b6ce357005db38343b976ce
Lucas Gomes <lucasgms16@gmail.com>
2020-01-27 02:34:44 -0300

    Setting database.yml

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
+  #namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
+  namespace :api, defaults: { format: :json } do
 
   end
 end
