543ac3c092e10a855073eadd079ec33ff2fd04fc
Lucas Gomes <lucasgms16@gmail.com>
2020-05-12 22:50:55 -0300

    Refactoring tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -23,7 +23,7 @@ require 'database_cleaner'
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
+Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
@@ -57,6 +57,7 @@ RSpec.configure do |config|
 
   # Factory Girl config
   # config.include FactoryGirl::Syntax::METHODS
+  config.include RequestSpecHelper, type: :request
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
