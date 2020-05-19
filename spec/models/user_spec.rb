3f1d8d6de10d0e820d0bf8f02e920d8793c495a8
Lucas Gomes <lucasgms16@gmail.com>
2020-05-18 23:47:31 -0300

    Adding auth

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,6 +6,13 @@ RSpec.describe User, type: :model do
   it { is_expected.to validate_presence_of(:email) }
   it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
   it { is_expected.to validate_confirmation_of(:password) }
-  it { is_expected.to allow_value("lucas@gomes.com").for(:email) }
+  it { is_expected.to allow_value('lucas@gomes.com').for(:email) }
+  it { is_expected.to validate_uniqueness_of(:auth_token) }
 
+  describe '#info' do
+    it 'returns email and created_t' do
+      user.save!
+      expect(user.info).to eq("#{user.email} - #{user.created_at}")
+    end
+  end
 end
