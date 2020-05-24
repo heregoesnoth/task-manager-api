8031c92779e68bfda9b8033287de524b4ecde75b
Lucas Gomes <lucasgms16@gmail.com>
2020-05-23 22:04:38 -0300

    Adding session login

--- spec/requests/api/v1/sessions_spec.rb
+++ spec/requests/api/v1/sessions_spec.rb
@@ -5,8 +5,8 @@ RSpec.describe 'Sessions API', type: :request do
   let!(:user) { create(:user) }
   let(:headers) do
     {
-        :Accept => 'application/vnd.taskmanager.v1',
-        'Content-Type' => Mime[:json].to_s
+      :Accept => 'application/vnd.taskmanager.v1',
+      'Content-Type' => Mime[:json].to_s
     }
   end
 
@@ -16,14 +16,30 @@ RSpec.describe 'Sessions API', type: :request do
     end
 
     context 'when the credentials are correct' do
-      let(:credentials) { {email: user.email, password: '123456'} }
+      let(:credentials) { { email: user.email, password: '123456' } }
+
       it 'returns status code 200' do
         expect(response).to have_http_status(200)
       end
 
-      it 'returns the json data for the user with auth roken'
-      expect(json_body[:auth_token]).to eq(user.auth_token)
+      it 'returns the json data for the user with auth roken' do
+        user.reload
+        expect(json_body[:auth_token]).to eq(user.auth_token)
+      end
     end
+
+    context 'when the credentials are incorrect' do
+      let(:credentials) { { email: user.email, password: 'invalid_password' } }
+
+      it 'returns status code 401' do
+        expect(response).to have_http_status(401)
+      end
+
+      it 'returns the json data for the user with auth roken' do
+        expect(json_body).to have_key(:errors)
+      end
+    end
+
   end
 
 end
